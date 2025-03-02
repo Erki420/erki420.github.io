e51258609545cb68177488b76fa711afb3af974e
Erki <marju9777@gmail.com>
2025-03-02 02:24:47 +0200

    .

---
+++ Javascripts/esileht.js
@@ -0,0 +1,22 @@
+const tuvastatudTekst = document.getElementById("seadmetekst")
+
+function KasutajaOnTelefonis() {
+    const kasutaja = navigator.userAgent.toLowerCase();
+    if (/mobile/i.test(kasutaja)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function TuvastaTelefon() {
+    if (KasutajaOnTelefonis){
+        tuvastatudTekst.textContent = "Tuvastatud telefon";
+    }
+    else{
+        tuvastatudTekst.textContent = "Tuvastatud arvuti";
+    }
+
+    requestAnimationFrame(TuvastaTelefon());
+}
