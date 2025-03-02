bdbf1e86dc1d90d3745621bf8e1a8a9f2a09b948
Erki <marju9777@gmail.com>
2025-03-02 03:05:21 +0200

    Update esileht.js

--- Javascripts/esileht.js
+++ Javascripts/esileht.js
@@ -1,20 +1,24 @@
-window.onload = function() {
-    const tuvastatudTekst = document.getElementById("seadmetekst");
+const tuvastatudTekst = document.getElementById("seadmetekst")
 
-    function KasutajaOnTelefonis() {
-        const kasutaja = navigator.userAgent.toLowerCase();
-        return /mobile/i.test(kasutaja); // Lihtsustatud tagastamine
+function KasutajaOnTelefonis() {
+    const kasutaja = navigator.userAgent.toLowerCase();
+    if (/mobile/i.test(kasutaja)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function TuvastaTelefon() {
+    if (KasutajaOnTelefonis()){
+        tuvastatudTekst.textContent = "Tuvastatud telefon";
+    }
+    else{
+        tuvastatudTekst.textContent = "Tuvastatud arvuti";
     }
 
-    function TuvastaTelefon() {
-        if (KasutajaOnTelefonis()) {
-            tuvastatudTekst.textContent = "Tuvastatud telefon";
-        } else {
-            tuvastatudTekst.textContent = "Tuvastatud arvuti";
-        }
+    requestAnimationFrame(TuvastaTelefon);
+}
 
-        requestAnimationFrame(TuvastaTelefon); // Kutsume iga kaadriga
-    }
-
-    requestAnimationFrame(TuvastaTelefon); // Algatame tuvastamise
-};
+requestAnimationFrame(TuvastaTelefon);
