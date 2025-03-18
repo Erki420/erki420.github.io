030713ae0ad4cf94c8e7afce8bcd6c08c3d6f54d
Erki <marju9777@gmail.com>
2025-03-18 16:24:08 +0200

    .

--- Javascripts/esileht.js
+++ Javascripts/esileht.js
@@ -1,19 +1,8 @@
-const tuvastatudTekst = document.getElementById("seadmetekst")
-const tuvstamisNupp = document.getElementById("nupp")
-
-function KasutajaOnTelefonis() {
-    const kasutaja = navigator.userAgent.toLowerCase();
-return /Android/iPhone/i.test(navigator.userAgent.toLowerCase)
-
+window.onload = function(){
+    teata();
 }
-console.log(KasutajaOnTelefonis())
-console.log(KasutajaOnTelefonis)
-tuvastamisNupp.onclick = TuvastaTelefon;
-function TuvastaTelefon() {
-    if (KasutajaOnTelefonis()){
-        tuvastatudTekst.textContent = "Tuvastatud telefon";
-    }
-    else{
-        tuvastatudTekst.textContent = "Tuvastatud arvuti";
-    }
+
+function teata(){
+    print("Update jooksis!");
+    requestAnimationFrame(teata);
 }
